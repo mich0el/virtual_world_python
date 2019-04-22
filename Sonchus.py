a2733f8e8b448f18c53a784aa3be0489f57cbd57
mich0el <49839366+mich0el@users.noreply.github.com>
2019-04-22 13:05:31 +0200

    Update Sonchus.py

--- Sonchus.py
+++ Sonchus.py
@@ -1,7 +1,7 @@
 from Plant import Plant
 
-class Sonchus(Plant):
 
+class Sonchus(Plant):
     def __init__(self, posX, posY, world):
         super().__init__()
         self.myColor = (255, 255, 51)
